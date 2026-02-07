576c2e91f201eae261543e81e4769677602f94d0
Nikita Fenyak <nefenyak92@yandex.ru>
2026-02-08 01:47:03 +0300

    test

--- src/main.js
+++ src/main.js
@@ -1,28 +1,29 @@
 import './fonts/ys-display/fonts.css'
 import './style.css'
 
-import {data as sourceData} from "./data/dataset_1.js";
+import { data as sourceData } from './data/dataset_1.js'
 
-import {initData} from "./data.js";
-import {processFormData} from "./lib/utils.js";
+import { initData } from './data.js'
+import { processFormData } from './lib/utils.js'
 
-import {initTable} from "./components/table.js";
+import { initTable } from './components/table.js'
 // @todo: подключение
 
+// test
 
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+const { data, ...indexes } = initData(sourceData)
 
 /**
  * Сбор и обработка полей из таблицы
  * @returns {Object}
  */
 function collectState() {
-    const state = processFormData(new FormData(sampleTable.container));
+  const state = processFormData(new FormData(sampleTable.container))
 
-    return {
-        ...state
-    };
+  return {
+    ...state,
+  }
 }
 
 /**
@@ -30,25 +31,26 @@ function collectState() {
  * @param {HTMLButtonElement?} action
  */
 function render(action) {
-    let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
-    // @todo: использование
+  let state = collectState() // состояние полей из таблицы
+  let result = [...data] // копируем для последующего изменения
+  // @todo: использование
 
-
-    sampleTable.render(result)
+  sampleTable.render(result)
 }
 
-const sampleTable = initTable({
+const sampleTable = initTable(
+  {
     tableTemplate: 'table',
     rowTemplate: 'row',
     before: [],
-    after: []
-}, render);
+    after: [],
+  },
+  render
+)
 
 // @todo: инициализация
 
+const appRoot = document.querySelector('#app')
+appRoot.appendChild(sampleTable.container)
 
-const appRoot = document.querySelector('#app');
-appRoot.appendChild(sampleTable.container);
-
-render();
+render()
